b4e512a7a25c74691d54a57d018d236c386becf3
youknowwhoiam1 <workwithanshumandas@gmail.com>
2024-02-01 23:07:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+document.getElementById('questionForm').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const question = document.getElementById('question').value;
+  const responseElement = document.getElementById('response');
+
+  try {
+    const answer = await getAnswer(question);
+
+    responseElement.textContent = answer;
+  } catch (error) {
+    console.error('Error:', error);
+    responseElement.textContent = 'An error occurred. Please try again.';
+  }
+});
+
+const getAnswer = async (text) => {
+  try {
+    const API_KEY = 'AIzaSyBbGD3N_YzG-zZSyq4I445J7PCx9agX6v0';
+
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`;
+    const data = {
+      contents: { parts: { text } }
+    }
+
+    const options = {
+      method: 'POST',
+      headers,
+      url,
+      data
+    }
+
+    const response = await axios(options);
+    let responseOfQuery = response?.data?.candidates[0]?.content?.parts[0]?.text;
+    return responseOfQuery;
+  } catch (error) {
+    console.log(error?.response?.data || error?.response || error);
+  }
+}
