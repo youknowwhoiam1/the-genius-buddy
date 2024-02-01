927f09f23370a801ddd06a3cffbe522b01addee3
youknowwhoiam1 <workwithanshumandas@gmail.com>
2024-02-02 01:33:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,24 +17,23 @@ document.getElementById('questionForm').addEventListener('submit', async functio
 const getAnswer = async (text) => {
   try {
 
-    const netlifyUrl = 'https://the-genius-buddy.netlify.app/.netlify/functions/apiKey';
-    const netlifyOptions = {
-      method: 'GET',
-      netlifyUrl
-    }
-    let netlifyResponse = await axios(netlifyOptions);
-    console.log('1', netlifyResponse);
-    netlifyResponse = netlifyResponse?.body;
-    console.log('2', netlifyResponse);
-    netlifyResponse = JSON.parse(netlifyResponse);
+    // const netlifyUrl = 'https://the-genius-buddy.netlify.app/.netlify/functions/apiKey';
+    // const netlifyOptions = {
+    //   method: 'GET',
+    //   netlifyUrl
+    // }
+    // let netlifyResponse = await axios(netlifyOptions);
+    // console.log('1', netlifyResponse);
+    // netlifyResponse = netlifyResponse?.body;
+    // console.log('2', netlifyResponse);
+    // netlifyResponse = JSON.parse(netlifyResponse);
 
-    const API_KEY = netlifyResponse?.API_KEY;
-    console.log(API_KEY);
+    const geminiApiKey = process.env.API_KEY;
 
     const headers = {
       'Content-Type': 'application/json'
     };
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${geminiApiKey}`;
     const data = {
       contents: { parts: { text } }
     }
